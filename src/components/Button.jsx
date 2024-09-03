7cb6050766809d4312268bd727e3accd00754a7b
Your Name <douglasnyabasa400@gmail.com>
2024-09-03 12:42:29 +0200

    first commit1

---
+++ src/components/Button.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Button = ({label, iconUrl,background,borderColor, fullWidth,textColor}) => {
+  return (
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        background
+          ? `${background} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
+      {label}
+
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt='arrow right icon'
+          className='ml-2 rounded-full bg-white w-5 h-5'
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Service from "./Service";
+
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Subscribe,
+  Service,
+  SpecialOffer,
+  CustomerReviews,
+  Footer
+}
